dcf90a62efbf8b6c2d268a9b674391bc0ff3eefe
wangkevin20 <71090255+wangkevin20@users.noreply.github.com>
2020-11-20 11:11:22 -0500

    week 10

---
+++ week10ex1.js
@@ -0,0 +1,31 @@
+function add(type){
+
+var num1= parseInt(document.getElementById("num1").value);
+var num2= parseInt(document.getElementById("num2").value);
+
+document.getElementById("output").innerHTML = num1 + num2;
+}
+
+function subtract(type){
+
+var num1= parseInt(document.getElementById("num1").value);
+var num2= parseInt(document.getElementById("num2").value);
+
+document.getElementById("output").innerHTML = num1 - num2;
+}
+
+function muti(type){
+
+var num1= parseInt(document.getElementById("num1").value);
+var num2= parseInt(document.getElementById("num2").value);
+
+document.getElementById("output").innerHTML = num1 * num2;
+}
+
+function divide(type){
+
+var num1= parseInt(document.getElementById("num1").value);
+var num2= parseInt(document.getElementById("num2").value);
+
+document.getElementById("output").innerHTML = num1 / num2;
+}
